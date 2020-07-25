2ef85091fe618643e515735abf66591b025ea72d
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-07-25 20:20:39 +0530

    Added entry point files.

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './pdfs/Resume.pdf';
+
+import './images/ShashwatKathuria.jpg';
+import './images/drawing-canvas.png';
+import './images/social-cloud.png';
+import './images/serverless-chat-app.png';
+import './images/iitj-healthcare.png';
+import './images/flappy-bird.png';
+import './images/pong.png';
+import './images/data-structures-and-algorithms.png';
+import './images/cryptography.png';
+import './images/drag-vs-normal-projectile.png';
+import './images/drag-vs-normal-projectile-2.png';
+import './images/intelligent-traffic-monitoring-system.jpg';
+import './images/traffic-penalty-system.png';
+import './images/nlp-hindi-english.png';
+import './images/disaster-safety-android-app.png';
+import './images/disaster-safety-web-app.png';
+import './images/garage-stoplight.jpg';
+import './images/sidekiq.png';
+import './images/sidekiq.png';
+import './images/gsoc-outreachy-intro.png';
+import './images/gsoc-outreachy-update-meetup-1.png';
+import './images/selection-email.jpeg';
+import './images/gsoc-first-phase-completed.png';
+import './images/sentry.png';
+import './images/gsoc.png';
+import './images/gsoc-2.png';
+import './images/gsoc-3.png';
+import './images/microsoft.jpg';
+import './images/microsoft-2.png';
+import './images/wikimedia.png';
+
+import './stylesheets/variables.scss';
+import './stylesheets/animations.scss';
+import './stylesheets/main.scss';
+import './stylesheets/footer_section.scss';
+import './stylesheets/resume_section.scss';
+import './stylesheets/home_section.scss';
+import './stylesheets/project_section.scss';
+import './stylesheets/blog_section.scss';
+import './stylesheets/navbar.scss';
+import './stylesheets/icons.scss';
+
+import ReactContainer from './js/components/react_container.jsx';
