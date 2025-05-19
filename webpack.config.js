78f453abd81a8d4d643030ce017dbfae55b34006
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2025-05-19 23:39:16 +0530

    Removed deprecated lib.

--- webpack.config.js
+++ webpack.config.js
@@ -21,17 +21,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ],
-            plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-transform-class-properties"
-            ]
-          }
+          loader: "babel-loader"
         }
       },
       {
